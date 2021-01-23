32ec772091835ffbc7a4bfa27f8ab1d5f43bbbf9
RENECANTU <rene.cantu.ga@usb.edu.mx>
2021-01-23 11:34:45 -0600

    Botón de construcción
    Se atendió el botón de artención y ventana adicional de regresa pronto

---
+++ js/boton.js
@@ -0,0 +1,11 @@
+function botonProximamente() {
+
+
+
+    //en el alert escribirá la frase "suma simple..." - se pone "number" para que lo reconozca como numero
+
+
+
+    // se concatena con una "coma" 
+    alert("Estamos en construcción, regresa pronto");
+}
